47110a750bf558fcfde94c724884d29cf2df789d
Red One <saxovtsfactory16@gmail.com>
2024-01-30 12:36:29 +0100

    First

---
+++ src/main/java/com/example/bibliotecaspringboot/BibliotecaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibliotecaspringboot/BibliotecaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
