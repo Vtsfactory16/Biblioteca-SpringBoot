d23982b450ef5f36dde8ded980fc8dc1b9aba255
Iridescent <macortes1010@ieszaidinvergeles.org>
2024-02-06 10:45:42 +0100

    libro

--- API/src/main/java/com/example/bibliotecaspringboot/controllers/ControllerLibro.java
+++ API/src/main/java/com/example/bibliotecaspringboot/controllers/ControllerLibro.java
@@ -1,4 +1,36 @@
 package com.example.bibliotecaspringboot.controllers;
 
+import com.example.bibliotecaspringboot.models.entities.LibroDTO;
+import com.example.bibliotecaspringboot.models.repositories.IRepositoryLibro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/biblioteca/libro")
 public class ControllerLibro {
+    @Autowired
+    IRepositoryLibro repositoryLibro;
+
+
+    @GetMapping
+    public List<LibroDTO> buscarEmpleados() {
+        return (List<LibroDTO>) repositoryLibro.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<LibroDTO> buscarEmpleadoPorId(@PathVariable(value = "id") int id) {
+        Optional<LibroDTO> libro = repositoryLibro.findById(id);
+        if (libro.isPresent())
+            return  ResponseEntity.ok().body(libro.get());
+        else return ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    public LibroDTO guardarEmpleado(@Validated @RequestBody LibroDTO libro) {
+        return repositoryLibro.save(libro);
+    }
+
 }
--- API/src/main/java/com/example/bibliotecaspringboot/models/repositories/IRepositoryLibro.java
+++ API/src/main/java/com/example/bibliotecaspringboot/models/repositories/IRepositoryLibro.java
@@ -1,4 +1,17 @@
 package com.example.bibliotecaspringboot.models.repositories;
 
-public interface IRepositoryLibro {
+import com.example.bibliotecaspringboot.models.entities.LibroDTO;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+@Repository
+public interface IRepositoryLibro extends CrudRepository<LibroDTO, Integer> {
+
+
 }
