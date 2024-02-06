8dc69d8d43aab775053c50b6a65829a22d5d527b
cakeneka <nekaelia@protonmail.com>
2024-02-06 10:52:49 +0100

    Controller usuario

--- API/src/main/java/com/example/bibliotecaspringboot/models/repositories/IRepositoryUsuario.java
+++ API/src/main/java/com/example/bibliotecaspringboot/models/repositories/IRepositoryUsuario.java
@@ -1,4 +1,11 @@
 package com.example.bibliotecaspringboot.models.repositories;
 
-public interface IRepositoryUsuario {
+import com.example.bibliotecaspringboot.models.entities.UsuarioDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRepositoryUsuario extends CrudRepository<UsuarioDTO, Integer> {
+
 }
