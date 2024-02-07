367e3b1a67a0e96248f55f70a8a0c4afbf78d285
Iridescent <macortes1010@ieszaidinvergeles.org>
2024-02-07 08:25:00 +0100

    commit irrelevante

--- API/src/main/java/com/example/bibliotecaspringboot/controllers/ControllerLibro.java
+++ API/src/main/java/com/example/bibliotecaspringboot/controllers/ControllerLibro.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/biblioteca/libro")
+@RequestMapping("biblioteca/libro")
 public class ControllerLibro {
     @Autowired
     IRepositoryLibro repositoryLibro;
