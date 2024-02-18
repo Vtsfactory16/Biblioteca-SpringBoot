458918d503da801c87ddfa41a8d30ca6a96da5b7
cakeneka <nekaelia@protonmail.com>
2024-02-18 07:56:38 +0100

    Añadir librería flatlaf
    Para mejorar fácilmente el aspecto de la aplicación

--- Client/src/Biblioteca.java
+++ Client/src/Biblioteca.java
@@ -1,9 +1,11 @@
+import com.formdev.flatlaf.FlatDarkLaf;
 import vista.FormMain;
 
 import javax.swing.*;
 
 public class Biblioteca {
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
         SwingUtilities.invokeLater(() -> FormMain.getInstance().setVisible(true));
     }
 }
